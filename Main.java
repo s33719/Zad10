b427cb45fa8496e7c1c01760fa979549c287b2c4
s33719 <s33719@pjwstk.edu.pl>
2025-06-09 18:13:28 +0200

    Zadanie10

--- Main.java
+++ Main.java
@@ -4,16 +4,14 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         KontenerNaPlyny k = new KontenerNaPlyny(1.0, 10, 10, 10, 1000.0, false);
-        KontenerNaPlyny k2 = new KontenerNaPlyny(1.0, 10, 10, 10, 1000.0, false);
-        KontenerNaPlyny k3 = new KontenerNaPlyny(1.0, 10, 10, 10, 1000.0, false);
-        KontenerNaPlyny k4 = new KontenerNaPlyny(1.0, 10, 10, 10, 1000.0, false);
-        KontenerNaPlyny k5 = new KontenerNaPlyny(1.0, 10, 10, 10, 1000.0, false);
+        KontenerNaGaz k2 = new KontenerNaGaz(5.0, 20, 50, 10, 1000.0, 1000);
+        KontenerNaPlyny k3 = new KontenerNaPlyny(1.0, 10, 10, 10, 1000.0, true);
+        KontenerChlodniczy k4 = new KontenerChlodniczy(5.0, 20, 50, 10, 1000.0, 38);
         System.out.println(k);
         System.out.println(k2);
-
+        Statek statek1 = new Statek(10.0, 50, 10000, "Statek1");
+        Statek statek2 = new Statek(10.0, 50, 10000, "Statek2");
         k.zaladujLadunek(100);
-        Statek statek1 = new Statek(10.0, 100, 40000, "statek1");
-        Statek statek2 = new Statek(10.0, 100, 40000, "statek2");
         statek1.zaladujKontener(k);
         List<Kontener> l1 = new ArrayList<>();
         l1.add(k2);
@@ -23,11 +21,9 @@ public class Main {
         k.oproznijLadunek();
         statek1.zastapKontenery("KON-L-3", k4);
         statek1.przeniesKontener(statek1, statek2, k2);
-        statek1.zaladujKontener(k5);
         statek1.getInfo();
-        System.out.println("                                 ");
-        System.out.println("                                 ");
-
+        System.out.println();
+        System.out.println();
         statek2.getInfo();
 
     }
